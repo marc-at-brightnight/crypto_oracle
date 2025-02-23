9e0a343265d559325daa6fed6be6509ff6d54fef
Marc Hermann <76454594+marc-at-brightnight@users.noreply.github.com>
2025-02-23 06:15:57 -0500

    Add formatting and UV (#1)

--- src/models/predict.py
+++ src/models/predict.py
@@ -1,33 +1,47 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 import numpy as np
 import pandas as pd
-from tensorflow.keras.models import load_model # type: ignore
-from tensorflow.keras.optimizers import Adam # type: ignore
+from tensorflow.keras.models import load_model  # type: ignore
+from tensorflow.keras.optimizers.legacy import Adam  # type: ignore
 from utils.project_functions import load_data, f1_score
 from data.data_preparation import load_preprocessed_data
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score as skl_f1_score, roc_auc_score, matthews_corrcoef
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score as skl_f1_score,
+    roc_auc_score,
+    matthews_corrcoef,
+)
 
 
 def predict(model_path, data_path, timesteps):
     # Load the preprocessed data
-    X_train, X_test, y_train, y_test, time_test, price, scaler = load_preprocessed_data(data_path)
-    
+    X_train, X_test, y_train, y_test, time_test, price, scaler = load_preprocessed_data(
+        data_path
+    )
+
     # Load the model without compiling it
     model = load_model(model_path, compile=False)
-    
+
     # Compile the model with custom objects
-    model.compile(optimizer=Adam(learning_rate=0.001), loss='binary_crossentropy', metrics=['accuracy', 'precision', 'recall', f1_score])
-    
+    model.compile(
+        optimizer=Adam(learning_rate=0.001),
+        loss="binary_crossentropy",
+        metrics=["accuracy", "precision", "recall", f1_score],
+    )
+
     # Make predictions
     y_pred_prob = model.predict(X_test)
     y_pred = (y_pred_prob >= 0.5).astype(int)
-    
+
     # Ensure y_test and y_pred are binary arrays
     y_test = y_test.astype(int)
     y_pred = y_pred.astype(int)
-        
+
     print("Accuracy:", accuracy_score(y_test, y_pred))
     print("Precision:", precision_score(y_test, y_pred))
     print("Recall:", recall_score(y_test, y_pred))
--- src/utils/project_functions.py
+++ src/utils/project_functions.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import tensorflow as tf
-from tensorflow.keras import backend as K # type: ignore
+from tensorflow.keras import backend as K  # type: ignore
+
 
 def load_data(file_path):
     """
@@ -13,28 +14,30 @@ def load_data(file_path):
     except Exception as e:
         print(f"Error loading data: {e}")
         return None
-    
+
+
 def reset_random_seeds(seed=1):
     import os
     import random
     import numpy as np
     import tensorflow as tf
-    
-    os.environ['PYTHONHASHSEED'] = str(seed)
+
+    os.environ["PYTHONHASHSEED"] = str(seed)
     tf.random.set_seed(seed)
     np.random.seed(seed)
     random.seed(seed)
 
+
 def f1_score(y_true, y_pred):
-    y_true = K.cast(y_true, 'float32')
-    y_pred = K.cast(y_pred, 'float32')
-    
+    y_true = K.cast(y_true, "float32")
+    y_pred = K.cast(y_pred, "float32")
+
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
     predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-    
+
     precision = true_positives / (predicted_positives + K.epsilon())
     recall = true_positives / (possible_positives + K.epsilon())
-    
+
     f1_val = 2 * (precision * recall) / (precision + recall + K.epsilon())
     return f1_val
